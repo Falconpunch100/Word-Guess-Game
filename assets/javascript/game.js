228d6f6de746b178c748a4eb3f28a0595aef6032
Jordan <jordanjubenville@hotmail.com>
2019-12-16 18:54:54 -0700

    starting off

---
+++ assets/javascript/game.js
@@ -0,0 +1,29 @@
+var ThreeHouseshangman = {
+
+
+
+
+}
+
+
+
+
+
+
+    CorrectLetter = 
+
+    WrongLetter =
+
+document.onkeyup = function(event) {
+    var letter = event.key.toUpperCase();
+
+    if (letter === CorrectLetter) {
+
+    }
+
+
+    else (letter === WrongLetter) ;{
+        
+
+    }
+  };
